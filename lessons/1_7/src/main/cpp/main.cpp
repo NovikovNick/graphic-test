4f0528c75641ba4da5810eee91f33c70a2017d00
NovikovNick <nicknovikov10@gmail.com>
2018-06-02 20:28:40 +0300

    add lesson 1.7

---
+++ lessons/1_7/src/main/cpp/main.cpp
@@ -0,0 +1,183 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Graphic.h"
+
+GLFWwindow *window;
+bool isWireFrameState = false;
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 position;\n"
+                                   "layout (location = 1) in vec3 color;\n"
+                                   "out vec3 ourColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "gl_Position = vec4(position, 1.0);\n"
+                                   "ourColor = color;\n"
+                                   "}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 color;\n"
+                                     "in vec3 ourColor;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "color = vec4(ourColor, 1.0f);\n"
+                                     "}\n\0";
+
+//it is better to use GL primitive type instead predefined
+const GLuint WIDTH = 1024, HEIGHT = 768;
+
+void handleESC(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+void handleWireSwitch(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+
+int main() {
+
+    window = Graphic::initGLFW(WIDTH, HEIGHT);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, handleESC);
+    glfwSetKeyCallback(window, handleWireSwitch);
+
+
+    Graphic::initGLAD();
+
+
+    // Define the viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    GLint nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+
+    GLuint shaderProgram = glCreateProgram();
+    Graphic::registerShader(
+            shaderProgram,
+            "..\\lessons\\1_7\\src\\main\\glsl\\shader.vs.glsl",
+            "..\\lessons\\1_7\\src\\main\\glsl\\shader.frag.glsl"
+    );
+
+    GLfloat vertices[] = {
+            // Позиции          // Цвета             // Текстурные координаты
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
+    };
+    GLuint indices[] = {  // Помните, что мы начинаем с 0!
+            0, 1, 3,   // Первый треугольник
+            1, 2, 3    // Второй треугольник
+    };
+
+    /*float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);*/
+
+
+
+
+
+
+    GLuint texture = Graphic::loadTexture("..\\assets\\cube.jpg");
+    GLuint texture2 = Graphic::loadTexture("..\\assets\\awesomeface.png");
+
+    //create vertex buffer object and vertex array object
+    GLuint VBO, VAO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+// 3. Устанавливаем указатели на вершинные атрибуты
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *) 0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+
+
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        GLfloat timeValue = glfwGetTime();
+
+        //GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUseProgram(shaderProgram);
+        //glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture1"), 0);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture2"), 1);
+
+        glm::mat4 trans;
+        trans = glm::scale(trans, glm::vec3(0.2f + abs(cos(timeValue)), 0.2f +  abs(cos(timeValue)), 0.5));
+        trans = glm::rotate(trans, timeValue * 100.0f, glm::vec3(0.0, 0.0, 1.0));
+
+        GLuint transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    // Terminates GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+}
+
+void handleESC(GLFWwindow *window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void handleWireSwitch(GLFWwindow *window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_F1 && action == GLFW_PRESS) {
+        if (isWireFrameState) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        } else {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        isWireFrameState = !isWireFrameState;
+    }
+}
+
